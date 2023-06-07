8ed4eb761a373307885c1c74b8757b7a721eddac
Predator <eglebabachinaite123@gmail.com>
2023-06-07 15:19:39 +0300

    Added header for each table, the sum of a total price, company name and logo

--- main.py
+++ main.py
@@ -41,5 +41,24 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add finalizing sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum} Euros", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=15, style="B")
+    pdf.set_text_color(211,146,216)
+    pdf.cell(w=32, h=10, txt=f"UnicornCat")
+    pdf.image("uni_cat.png", w=10)
+
     pdf.output(f"PDFs/{filename}.pdf")
 
